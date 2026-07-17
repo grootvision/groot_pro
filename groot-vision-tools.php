620128d352a2d1a7d0dc2560dda2ad4c5420c241
mobin <uiqow.mobin@gmail.com>
2026-07-17 19:24:39 +0330

    بهبود

--- groot-vision-tools.php
+++ groot-vision-tools.php
@@ -20,16 +20,11 @@ define('GV_TOOLS_PATH', plugin_dir_path(__FILE__));
 define('GV_TOOLS_URL', plugin_dir_url(__FILE__));
 define('GV_TOOLS_VERSION', '3.0.1'); // هر بار نسخه جدید منتشر می‌کنید، این عدد را هم مثل بالای فایل تغییر دهید
 /*
-|--------------------------------------------------------------------------
-| لود داشبورد اصلی
-|--------------------------------------------------------------------------
-*/
-require_once GV_TOOLS_PATH . 'admin/admin.php';
-/*
-|--------------------------------------------------------------------------
+|--------------------------------------------------
 | لود ماژول‌ها
-|--------------------------------------------------------------------------
+|--------------------------------------------------
 */
+
 require_once GV_TOOLS_PATH . 'modules/add_cart.php';
 require_once GV_TOOLS_PATH . 'modules/notification.php';
 require_once GV_TOOLS_PATH . 'modules/progress_bar.php';
@@ -37,13 +32,22 @@ require_once GV_TOOLS_PATH . 'modules/speed_and_security.php';
 require_once GV_TOOLS_PATH . 'modules/topbar_news.php';
 require_once GV_TOOLS_PATH . 'modules/table_styles.php';
 require_once GV_TOOLS_PATH . 'modules/seo_keywords.php';
-require_once GV_TOOLS_PATH . 'modules/github_updater.php';   // به‌روزرسانی خودکار از گیت‌هاب
-require_once GV_TOOLS_PATH . 'modules/login_customizer.php'; // طراحی صفحه ورود وردپرس
-require_once GV_TOOLS_PATH . 'modules/font_manager.php';     // مدیریت فونت سایت
-require_once GV_TOOLS_PATH . 'modules/post_date_jalali.php'; // تاریخ خودکار شمسی روی عنوان
-require_once GV_TOOLS_PATH . 'modules/visitor_analytics.php'; // آمار بازدید و رفتار کاربران
-require_once GV_TOOLS_PATH . 'modules/maintenance.php'; // حالت تعمیر
-require_once GV_TOOLS_PATH . 'modules/groot-vision-tools.php'; // حالت تعمیر
+require_once GV_TOOLS_PATH . 'modules/github_updater.php';
+require_once GV_TOOLS_PATH . 'modules/login_customizer.php';
+require_once GV_TOOLS_PATH . 'modules/font_manager.php';
+require_once GV_TOOLS_PATH . 'modules/post_date_jalali.php';
+require_once GV_TOOLS_PATH . 'modules/visitor_analytics.php';
+require_once GV_TOOLS_PATH . 'modules/maintenance.php';
+
+/* فایل سیستم تیکت */
+require_once GV_TOOLS_PATH . 'modules/support_tickets.php';
+
+/*
+|--------------------------------------------------
+| لود داشبورد اصلی
+|--------------------------------------------------
+*/
+require_once GV_TOOLS_PATH . 'admin/admin.php';
 /*
 |--------------------------------------------------------------------------
 | پاکسازی هنگام غیرفعال‌سازی افزونه (فقط زمان‌بندی cron را پاک می‌کند،
